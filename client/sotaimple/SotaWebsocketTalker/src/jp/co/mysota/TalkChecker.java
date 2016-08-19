71a8d35ab42bfb55f514ee855b0f077dc5db5ebb
Yuta Koga <ykoga.kyutech@gmail.com>
2016-08-19 16:03:57 +0900

    changed TalkChecker to use talk.speechrecog.get block

--- client/sotaimple/SotaWebsocketTalker/src/jp/co/mysota/TalkChecker.java
+++ client/sotaimple/SotaWebsocketTalker/src/jp/co/mysota/TalkChecker.java
@@ -13,84 +13,39 @@ public class TalkChecker
 	public String speechRecogResult;
 	public RecogResult recogresult;
 	public jp.co.mysota.WebsocketMessenger WebsocketMessenger;
-	public void talkCheck()																								//@<BlockInfo>jp.vstone.block.func,64,192,832,192,False,8,@</BlockInfo>
+	public TalkChecker()																								//@<BlockInfo>jp.vstone.block.func.constructor,16,16,496,16,False,4,@</BlockInfo>
+	{
+																														//@<OutputChild>
+		/*String speechRecogResult*/;																					//@<BlockInfo>jp.vstone.block.variable,80,16,80,16,False,3,break@</BlockInfo>
+																														//@<EndOfBlock/>
+		/*RecogResult recogresult*/;																					//@<BlockInfo>jp.vstone.block.variable,144,16,144,16,False,2,break@</BlockInfo>
+																														//@<EndOfBlock/>
+		WebsocketMessenger=new jp.co.mysota.WebsocketMessenger();														//@<BlockInfo>jp.vstone.block.variable,208,16,208,16,False,1,break@</BlockInfo>
+																														//@<EndOfBlock/>
+																														//@</OutputChild>
+	}																													//@<EndOfBlock/>
+
+	//@<Separate/>
+	public void talkCheck()																								//@<BlockInfo>jp.vstone.block.func,64,192,576,192,False,9,@</BlockInfo>
 	throws SpeechRecogAbortException {
 		if(!GlobalVariable.TRUE) throw new SpeechRecogAbortException("default");
 
 																														//@<OutputChild>
-		WebsocketMessenger.connect((String)"");																			//@<BlockInfo>jp.vstone.block.callfunc.base,128,192,128,192,False,7,@</BlockInfo>	@<EndOfBlock/>
-		while(GlobalVariable.TRUE)																						//@<BlockInfo>jp.vstone.block.while.endless,288,192,736,192,False,6,Endless@</BlockInfo>
+		WebsocketMessenger.connect((String)"ws://52.198.53.205:8080/ws");												//@<BlockInfo>jp.vstone.block.callfunc.base,128,192,128,192,False,8,@</BlockInfo>	@<EndOfBlock/>
+		while(GlobalVariable.TRUE)																						//@<BlockInfo>jp.vstone.block.while.endless,288,192,480,192,False,7,Endless@</BlockInfo>
 		{
 
 																														//@<OutputChild>
-			recogresult = GlobalVariable.recog.getRecognitionwithAbort((int)60000);										//@<BlockInfo>jp.vstone.block.talk.speechrecog.score2,384,112,672,128,False,5,音声認識を行い、認識候補との完全一致で比較する。認識スコアが一番高い結果に分岐する。実際に認識された文字列はspeechRecogResultに代入される@</BlockInfo>
-			speechRecogResult = recogresult.CheckBest(new String[]{
-			 "こんにちは" ,  "ここの住所を教えて" ,  "出身地を教えて" ,  "" , 
-			},false);
+			speechRecogResult = GlobalVariable.recog.getResponsewithAbort((int)10000,(int)3);							//@<BlockInfo>jp.vstone.block.talk.speechrecog.get,352,192,352,192,False,6,音声認識して、得られた結果（文字列）をspeechRecogResultに代入します。@</BlockInfo>
 			if(speechRecogResult == null) speechRecogResult = "";
 
-			if(speechRecogResult.contains((String)"こんにちは"))
-			{
-				speechRecogResult = recogresult.getBasicResult();
-				if(speechRecogResult == null) speechRecogResult = "";
-
-																														//@<OutputChild>
-					GlobalVariable.sotawish.Say((String)"こんにちは",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);	//@<BlockInfo>jp.vstone.block.talk.say,496,144,496,144,False,1,@</BlockInfo>
-																															//@<EndOfBlock/>
-																																//@</OutputChild>
-
-			}
-			else if(speechRecogResult.contains((String)"ここの住所を教えて"))
-			{
-				speechRecogResult = recogresult.getBasicResult();
-				if(speechRecogResult == null) speechRecogResult = "";
-
-																														//@<OutputChild>
-					GlobalVariable.sotawish.Say((String)"新宿区西新宿8丁目17番1の新宿グランドタワーだよ。",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);	//@<BlockInfo>jp.vstone.block.talk.say,528,224,528,224,False,2,@</BlockInfo>
-																															//@<EndOfBlock/>
-																																//@</OutputChild>
-
-			}
-			else if(speechRecogResult.contains((String)"出身地を教えて"))
-			{
-				speechRecogResult = recogresult.getBasicResult();
-				if(speechRecogResult == null) speechRecogResult = "";
-
-																														//@<OutputChild>
-					GlobalVariable.sotawish.Say((String)"大阪かな",MotionAsSotaWish.MOTION_TYPE_TALK,(int)11,(int)13,(int)11);	//@<BlockInfo>jp.vstone.block.talk.say,512,320,512,320,False,3,@</BlockInfo>
-																															//@<EndOfBlock/>
-																																//@</OutputChild>
-
-			}
-			else
-			{
-				speechRecogResult = recogresult.getBasicResult();
-				if(speechRecogResult == null) speechRecogResult = "";
-
-																														//@<OutputChild>
-					WebsocketMessenger.sendMessage((String)speechRecogResult);												//@<BlockInfo>jp.vstone.block.callfunc.base,528,400,528,400,False,4,@</BlockInfo>	@<EndOfBlock/>
-																																//@</OutputChild>
-
-			}
 																														//@<EndOfBlock/>
+			WebsocketMessenger.sendMessage((String)speechRecogResult);													//@<BlockInfo>jp.vstone.block.callfunc.base,416,192,416,192,False,5,@</BlockInfo>	@<EndOfBlock/>
 																														//@</OutputChild>
 		}
 																														//@<EndOfBlock/>
 																														//@</OutputChild>
 
-	}																													//@<EndOfBlock/>
-
-	//@<Separate/>
-	public TalkChecker()																								//@<BlockInfo>jp.vstone.block.func.constructor,16,16,496,16,False,12,@</BlockInfo>
-	{
-																														//@<OutputChild>
-		/*String speechRecogResult*/;																					//@<BlockInfo>jp.vstone.block.variable,80,16,80,16,False,11,break@</BlockInfo>
-																														//@<EndOfBlock/>
-		/*RecogResult recogresult*/;																					//@<BlockInfo>jp.vstone.block.variable,144,16,144,16,False,10,break@</BlockInfo>
-																														//@<EndOfBlock/>
-		WebsocketMessenger=new jp.co.mysota.WebsocketMessenger();														//@<BlockInfo>jp.vstone.block.variable,208,16,208,16,False,9,break@</BlockInfo>
-																														//@<EndOfBlock/>
-																														//@</OutputChild>
 	}																													//@<EndOfBlock/>
 
 }
